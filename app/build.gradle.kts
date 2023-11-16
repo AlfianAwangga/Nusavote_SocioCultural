9a3d19ddf1db04c4efaeb3c92198b87768187571
awangga01 <alfianawangga15@gmail.com>
2023-11-16 13:15:11 +0900

    Menambahkan tema dan style aplikasi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,23 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // loopj
+    implementation("com.loopj.android:android-async-http:1.4.9")
+    // data store
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    // lottie
+    implementation("com.airbnb.android:lottie:4.2.0")
+    //konfetti
+//    implementation("nl.dionsegijn:konfetti:1.1.3")
+    // Ripple
+    implementation("com.skyfishjy.ripplebackground:library:1.0.1")
+    
+
+
 }
--- app/src/main/java/com/example/massive/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/massive/view/activities/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.massive.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import com.example.massive.R
+import com.example.massive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
