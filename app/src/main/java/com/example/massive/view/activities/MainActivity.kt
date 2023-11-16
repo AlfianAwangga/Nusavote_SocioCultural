164cfaf493033a3ef089a1c12f9679edaea33d1e
awangga01 <alfianawangga15@gmail.com>
2023-11-16 13:48:19 +0900

    Menambahkan logo dan splash screen

--- app/src/main/java/com/example/massive/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/massive/view/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.massive.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.massive.R
 import com.example.massive.databinding.ActivityMainBinding
 
@@ -10,8 +11,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        splashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    private fun splashScreen() {
+        Thread.sleep(1000)
+        installSplashScreen()
     }
 }
