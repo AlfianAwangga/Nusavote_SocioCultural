4209ad69d3e3585962eb45891768cadacea51b0e
Kristo Samosir <kristoisthebest@gmail.com>
2023-11-17 20:39:03 +0900

    menambahkan halaman login dan register, serta menambahkan logic confirm password dan button disable

--- app/src/main/java/com/example/massive/view/activities/LoginActivity.kt
+++ app/src/main/java/com/example/massive/view/activities/LoginActivity.kt
@@ -1,12 +1,58 @@
 package com.example.massive.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.example.massive.R
+import com.example.massive.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+private lateinit var binding: ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.etUsernameLogin.addTextChangedListener(loginTextWatcher)
+        binding.etPasswordLogin.addTextChangedListener(loginTextWatcher)
+
+        binding.btnMasuk.setOnClickListener(this)
+        binding.ivBackLogin.setOnClickListener(this)
+    }
+
+    private var loginTextWatcher = object :TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            var username = binding.etUsernameLogin.text.toString().trim()
+            var password = binding.etPasswordLogin.text.toString().trim()
+
+            if (username.isNotEmpty() && password.isNotEmpty()){
+                binding.btnMasuk.isEnabled = true
+            }else{
+                binding.btnMasuk.isEnabled = false
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+
+    }
+
+    override fun onClick(b: View) {
+        when (b.id){
+            R.id.btn_masuk->{
+                val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.iv_back_login->{
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/massive/view/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/massive/view/activities/RegisterActivity.kt
@@ -1,12 +1,46 @@
 package com.example.massive.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doOnTextChanged
 import com.example.massive.R
+import com.example.massive.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+private lateinit var binding: ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener(this)
+        binding.ivBack.setOnClickListener(this)
+
+    }
+
+    override fun onClick(r: View) {
+        when (r.id){
+            R.id.btn_register->{
+
+                val password = binding.etPassword.text.toString()
+                val confirmPassword = binding.etConfirmPassword.text.toString()
+
+                if (password != confirmPassword) {
+                    binding.tilConfirmPassword.error = "Konfirmasi Password Salah, Coba Lagi!"
+                } else {
+                    binding.tilConfirmPassword.error = null
+                    val intent = Intent(this@RegisterActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            R.id.iv_back->{
+                val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
